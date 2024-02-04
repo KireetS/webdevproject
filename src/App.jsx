bc995e0c77888a2ee7b135fc502d8e4470beb6f2
ya3hrawat <yashrawatdelhi25@gmail.com>
2024-02-04 22:48:55 +0530

    navbar added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar.tsx'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
